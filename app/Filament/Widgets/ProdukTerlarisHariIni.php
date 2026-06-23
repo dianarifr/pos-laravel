08063e104965c6eef5796354fac8cedcda3f0b26
dian <dianarifr@gmail.com>
2026-06-23 11:09:01 +0700

    chore: hapus pencarian produk

--- app/Filament/Widgets/ProdukTerlarisHariIni.php
+++ app/Filament/Widgets/ProdukTerlarisHariIni.php
@@ -41,7 +41,6 @@ class ProdukTerlarisHariIni extends BaseWidget
             ->columns([
                 Tables\Columns\TextColumn::make('nama_barang')
                     ->label('Nama Produk')
-                    ->searchable(),
 
                 Tables\Columns\TextColumn::make('total_terjual')
                     ->label('Terjual')
